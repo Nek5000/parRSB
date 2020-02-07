83562b727ca3ec27f14c2c108ca5cdbedcf5e931
Thilina Rathnayake <thilinarmtb@gmail.com>
2020-02-06 22:07:23 -0600

    Build Nek5000 only once

--- tests/test.sh
+++ tests/test.sh
@@ -10,6 +10,13 @@ export LD_LIBRARY_PATH="${BUILDDIR}:${LD_LIBRARY_PATH}"
 export LD_LIBRARY_PATH="${EXADIR}/lib:${LD_LIBRARY_PATH}"
 export LD_LIBRARY_PATH="${EXASORTDIR}/lib:${LD_LIBRARY_PATH}"
 
+dir_resolve()
+{
+  cd "$1" 2>/dev/null || return $?  # cd to desired directory;
+  #if fail, quell any error messages but return exit status
+  echo "`pwd -P`" # output full, link-resolved path
+}
+
 function print_test_err(){
   echo "Test: $1, not ok."
 }
@@ -23,13 +30,17 @@ function exit_err(){
   exit 1
 }
 
-function clean_nek(){
-  rm -rf *.co2 *.f build.log makefile nek5000\
-    obj/ SESSION.NAME 2>/dev/null *.out *.err 2>/dev/null
+function setup_nek5000(){
+  # TODO: Copy a specific nek release
+  git clone https://github.com/Nek5000/Nek5000.git
+
+  cd ${NEK_SOURCE_ROOT}/tools
+  ./maketools genbox gencon
+  cd - 2>/dev/null 1>&2
 }
 
 function clean_con(){
-  rm *.co2 parCon *.out *.err *.run 2>/dev/null
+  rm *.co2 *.re2 2>/dev/null
 }
 
 function gen_box(){
@@ -40,91 +51,65 @@ EOF
 }
 
 function setup_test(){
-  cp ./bbbb/* ${NEK_SOURCE_ROOT}/bin/makenek $1/;
+  local TEST=$1
 
-  cd $1;
-  gen_box;
-  cd - 2>/dev/null 1>&2
-}
+  clean_con
 
-function setup_nek5000(){
-  # TODO: Copy a specific nek release
-  git clone https://github.com/Nek5000/Nek5000.git
+  cp ${BUILDDIR}/examples/parCon . 2>/dev/null
+  if [ ! -f ./parCon ]; then
+    exit_err "Error copying parCon."
+  fi
 
-  cd ${NEK_SOURCE_ROOT}/tools
-  ./maketools genbox gencon
-  cd - 2>/dev/null 1>&2
+  cp ${NEK_SOURCE_ROOT}/bin/makenek ${TEST}/genbox.in .
+  gen_box
+
+  ./makenek ${CASE} >build.out 2>build.err
+  if [ -s build.err ]; then
+    exit_err "Error building ${TEST}."
+  fi
 }
 
 function run_nek5000(){
   local CASE=$1
   local NP=$2
+  local TEST=$3
 
   echo ${CASE}   >  SESSION.NAME
   echo `pwd`'/' >>  SESSION.NAME
-  mpirun -np ${NP} ./nek5000 >nek.out 2>nek.err
+  mpirun -np ${NP} ./nek5000 >nek.${TEST}.out 2>nek.${TEST}.err
 }
 
 function run_test(){
   local TEST=$1
   local NP=$2
-  local ORIGDIR=`pwd`
 
-  cd ${TEST}
-
-  if [ ${VERBOSE} -eq 1 ]; then
-    echo "Building ${TEST} ..."
-  fi
-  ./makenek ${CASE} >build.out 2>build.err
-  if [ -s build.err ]; then
-    cd ${ORIGDIR}
-    exit_err "Error building ${TEST}."
-  fi
-
-  if [ ${VERBOSE} -eq 1 ]; then
-    echo "Copying parCon from ${BUILDDIR}/examples/parCon"
-  fi
-  cp ${BUILDDIR}/examples/parCon . 2>/dev/null
-  if [ ! -f ./parCon ]; then
-    cd ${ORIGDIR}
-    exit_err "Error copying parCon."
-  fi
-
-  if [ ${VERBOSE} -eq 1 ]; then
-    echo "Running parCon on ${CASE}.re2 ..."
-  fi
-  if [ ${NP} -gt 8 ]; then
-    NP=8
-  fi
-  mpirun -np ${NP} ./parCon ${CASE}.re2 \
-    >parcon.out 2>parcon.err
+  if [ ${NP} -gt 4 ]; then NP=4; fi
+  mpirun -np ${NP} ./parCon ${CASE}.re2 >parcon.out 2>parcon.err
   if [ -s parcon.err ]; then
-    cd ${ORIGDIR}
     exit_err "Error running parCon."
   fi
 
-  if [ ${VERBOSE} -eq 1 ]; then
-    echo "Running ${TEST} ..."
-  fi
-  run_nek5000 ${CASE} ${NP}
+  TEST="`dir_resolve \"${TEST}\"`"
+  TEST=`basename ${TEST}`
+  run_nek5000 ${CASE} ${NP} ${TEST}
 
-  success=`grep 'run successful: dying ...' nek.out | wc -l`
+  success=`grep 'run successful: dying ' nek.${TEST}.out | wc -l`
   if [ ${success} -eq 0 ]; then
     print_test_err "${TEST}"
   else
     print_test_success "${TEST}"
   fi
-
-  cd ${ORIGDIR}
 }
 
 function run_test_suite(){
-  for i in ./[bp]0*; do
+  cd bbbb
+  for i in ../[bp]0*; do
     setup_test $i
 
     NP=${i:(-3)};
     run_test ${i} ${NP}
   done
+  cd - 2>/dev/null 1>&2
 }
 
 while (( "$#" )); do
