f5446d3301ed2b0524549fd57c89d213fd8d0f26
Thilina Rathnayake <thilinarmtb@gmail.com>
2020-03-21 02:59:03 -0500

    WIP: parRCB

--- src/parRCB.c
+++ src/parRCB.c
@@ -12,9 +12,9 @@ void fparRCB_partMesh(int *part,double *vtx,int *nel,int *ndim,
 {
   *err = 1;
 
-  GenmapCommExternal c;
+  exaCommExternal c;
   c = MPI_Comm_f2c(*comm);
-  *err = parRCB_partMesh(part,vtx,*nel,*ndim,options,c);
+  *err=parRCB_partMesh(part,vtx,*nel,*ndim,options,c);
 }
 
 int parRCB_partMesh(int *part,double *vtx,int nel,int ndim,
@@ -36,60 +36,58 @@ int parRCB_partMesh(int *part,double *vtx,int nel,int ndim,
   exaScan(h,&nelg_start,&nell,&buf0,1,exaLong_t,exaAddOp);
   nelg_start-=nell;
 
-  struct array eList;
-  elm_rcb *data;
+  exaArray eList; exaArrayInit(&eList,elm_rcb,nel);
+  elm_rcb *data=exaArrayGetPointer(eList);
 
-  array_init(elm_rcb,&eList,nel), eList.n=nel;
   int e, n;
-  for(data=eList.ptr,e=0; e<nel; ++e) {
+  for(e=0;e<nel;++e){
     data[e].id=nelg_start+(e+1);
     const exaLong eg=data[e].id;
 
     data[e].proc=(int) ((eg-1)/nstar);
+    data[e].orig=rank;
     if(eg>size*nstar) data[e].proc= (eg%size)-1; 
     for(int n=0;n<ndim;n++)
       data[e].coord[n]=vtx[e*ndim+n];
   }
+  exaArraySetSize(eList,nel);
 
-  struct comm c; comm_init(&c,comm);
-  struct crystal cr; crystal_init(&cr,&c);
   buffer buf; buffer_init(&buf,1024);
 
-  sarray_transfer(elm_rcb,&eList,proc,1,&cr);
-  data=eList.ptr;
-  nel =eList.n;
+  exaArrayTransfer(eList,offsetof(elm_rcb,proc),1,exaGetComm(h));
+  exaSortArray(eList,exaLong_t,offsetof(elm_rcb,id));
 
-  sarray_sort(elm_rcb,data,(unsigned int)nel,id,TYPE_LONG,&buf);
+  exaComm commRcb;
+  nel=exaArrayGetSize(eList);
+  exaCommSplit(&commRcb,nel>0,rank);
 
-  MPI_Comm commRSB;
-  MPI_Comm_split(comm, nel>0, rank, &commRSB);
-
-  if(nel>0) {
+  if(nel>0){
     double time0 = comm_time();
     if(options!=NULL) exaSetDebug(h,options[0]);
     if(exaRank(h)==0)
       exaDebug(h,"\nfinished in %lfs\n",comm_time()-time0);
 
+    parRCB(h,eList);
+
     fflush(stdout);
   }
-
-  MPI_Comm_free(&commRSB);
+  exaCommDestroy(commRcb);
 
   /* restore original input */
-  sarray_transfer(elm_rcb,&eList,proc,0,&cr);
-  data=eList.ptr;
-  nel =eList.n;
-  sarray_sort(elm_rcb,data,(unsigned int)nel,id,TYPE_LONG,&buf);
-  MPI_Barrier(comm);
+  exaArrayTransfer(eList,offsetof(elm_rcb,orig),1,exaGetComm(h));
+  exaSortArray(eList,exaLong_t,offsetof(elm_rcb,id));
 
-  for(e = 0; e < nel; e++) {
-    part[e]=data[e].part;
+  exaBarrier(h);
+
+  data=exaArrayGetPointer(eList);
+  nel=exaArrayGetSize(eList);
+  for(e=0;e<nel;e++) {
+    part[e]=data[e].orig;
   }
 
-  array_free(&eList);
   buffer_free(&buf);
-  crystal_free(&cr);
-  comm_free(&c);
+
+  exaArrayFree(eList);
   exaFinalize(h);
 
   return 0;
