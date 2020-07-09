048a8615a974df68edcdaa85d029a6d519e7e2c8
Thilina Rathnayake <thilinarmtb@gmail.com>
2020-07-09 17:18:37 -0500

    WIP: Vcycle

--- src/genmap-multigrid-vcycle.c
+++ src/genmap-multigrid-vcycle.c
@@ -1,6 +1,12 @@
 #include <genmap-impl.h>
 #include <genmap-multigrid-precon.h>
 
-void mg_vcycle(GenmapScalar *rhs,mgData d,uint lvl){
-  assert(lvl<d->nLevels);
+void mg_vcycle(GenmapScalar *u,GenmapScalar *rhs,mgData d){
+
+  uint lvl;
+  for(lvl=0; lvl<d->nLevels-1; lvl++){
+    parMat M=d->levels[lvl]->M;
+    uint off=d->level_off[lvl];
+    uint n  =d->level_off[lvl+1]-off;
+  }
 }
