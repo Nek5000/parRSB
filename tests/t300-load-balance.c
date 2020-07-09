47d75e1b9a7f21a3fa9611299e4722e1fdb9d105
Thilina Rathnayake <thilinarmtb@gmail.com>
2020-07-09 07:39:53 -0500

    Add tests/t300 -- load balance

--- tests/t300-load-balance.c
+++ tests/t300-load-balance.c
@@ -18,14 +18,12 @@ int main(int argc,char *argv[]){
   struct array arr; array_init(Data,&arr,N);
 
   int i,cnt; Data d; Data *ptr=arr.ptr;
-  for(i=cnt=0; i<N; i++)
-      d.ds=(rand()%100)/100.0,ptr[cnt++]=d;
+  for(i=0; i<N; i++)
+      d.ds=(rand()%100)/100.0,ptr[i]=d;
+  arr.n=N;
 
-#if 0
-  exaSort(arr,exaScalar_t,offsetof(Data,ds),bin_sort,1,
-      exaGetComm(h));
-  assert(exaArrayGetSize(arr)==N);
-#endif
+  parallel_sort(Data,&arr,offsetof(Data,ds),gs_double,&c);
+  assert(arr.n==N);
 
   array_free(&arr);
 
