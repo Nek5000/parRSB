e4776b6543cf955b120c640cadbe5163ff83f03b
Thilina Rathnayake <thilinarmtb@gmail.com>
2022-09-24 18:21:01 -0400

    Implement multi source bfs to find components

--- src/components.c
+++ src/components.c
@@ -124,6 +124,205 @@ static uint get_components(sint *component, struct rsb_element *elements,
   return count;
 }
 
+struct cmp_t {
+  uint c, p, uid;
+};
+
+static sint find_or_insert(struct array *cids, struct cmp_t *t) {
+  // If there are no elements in the array, insert and exit
+  if (cids->n == 0) {
+    array_cat(struct cmp_t, cids, t, 1);
+    return -1;
+  }
+
+  // Otherwise, we will do a binary search
+  struct cmp_t *pc = (struct cmp_t *)cids->ptr;
+  sint s = 0, e = cids->n - 1, mid = 0;
+  while (s <= e) {
+    mid = (s + e) / 2;
+    if (t->c == pc[mid].c)
+      return pc[mid].uid;
+    else if (t->c < pc[mid].c)
+      e = mid - 1;
+    else // c > pc[mid].c
+      s = mid + 1;
+  }
+
+  // Okay, not found -- insert at `mid` or `mid + 1`
+  uint max = cids->max;
+  if (max == cids->n) {
+    max += max / 2 + 1;
+    array_reserve(struct cmp_t, cids, max);
+  }
+
+  uint n = mid;
+  if (t->c > pc[mid].c)
+    n = mid + 1;
+
+  struct cmp_t t0 = *t, t1;
+  for (; n < cids->n; n++) {
+    t1 = pc[n];
+    pc[n] = t0;
+    t0 = t1;
+  }
+  pc[n] = t0, cids->n++;
+
+  return -1;
+}
+
+static uint get_components_v2(sint *component, uint nelt, unsigned nv,
+                              struct rsb_element *elements,
+                              const struct comm *ci, buffer *bfr) {
+  slong out[2][1], wrk[2][1], in = nelt;
+  comm_scan(out, ci, gs_long, gs_add, &in, 1, wrk);
+  ulong nelg = out[1][0], start = out[0][0];
+
+  if (nelg == 0)
+    return 0;
+
+  uint nev = nelt * nv;
+  uint *inds = tcalloc(uint, nev);
+  sint *p = tcalloc(sint, nev);
+  slong *ids = tcalloc(slong, nev);
+
+  int null_input = (component == NULL);
+  if (null_input)
+    component = tcalloc(sint, nelt);
+
+  for (uint e = 0; e < nelt; e++)
+    component[e] = -1;
+
+  struct comm c;
+  slong nmkd = 0, nc = 0;
+  do {
+    // Copy unmarked elements to ids
+    uint unmkd = 0;
+    for (uint e = 0; e < nelt; e++) {
+      if (component[e] == -1) {
+        inds[unmkd] = e;
+        for (uint v = 0; v < nv; v++)
+          ids[unmkd * nv + v] = elements[e].vertices[v];
+        unmkd++;
+      }
+    }
+
+    int bin = (unmkd > 0);
+    comm_split(ci, bin, ci->id, &c);
+
+    slong nnzg = 0, nnzg0, ncg = 0;
+    if (bin == 1) {
+      // Setup gs
+      struct gs_data *gsh = gs_setup(ids, unmkd * nv, &c, 0, gs_pairwise, 0);
+
+      // Mark the first unmarked element as seed for the component c.id
+      for (uint v = 0; v < nv; v++)
+        p[0 * nv + v] = c.id;
+
+      // Initialize the rest of p
+      for (uint e = 1; e < unmkd; e++)
+        for (uint v = 0; v < nv; v++)
+          p[e * nv + v] = -1;
+
+      do {
+        nnzg0 = nnzg, nnzg = 0;
+
+        gs(p, gs_int, gs_max, 0, gsh, bfr);
+
+        for (uint e = 0; e < unmkd; e++) {
+          uint v = 0;
+          for (; v < nv; v++) {
+            if (p[e * nv + v] > -1) {
+              nnzg++;
+              break;
+            }
+          }
+
+          // There was one non-zero vertex in the element
+          if (v < nv) {
+            uint c = p[e * nv + v];
+            for (v = 0; v < nv; v++)
+              p[e * nv + v] = c;
+          }
+        }
+
+        comm_allreduce(&c, gs_long, gs_add, &nnzg, 1, wrk);
+      } while (nnzg0 < nnzg);
+      gs_free(gsh);
+
+      // Find unique local components and then use them to find unique
+      // global components
+      struct array cids;
+      array_init(struct cmp_t, &cids, 100);
+
+      struct cmp_t t;
+      for (uint e = 0; e < unmkd; e++) {
+        if (p[e * nv + 0] > -1) {
+          t.c = p[e * nv + 0], t.p = t.c % c.np;
+          find_or_insert(&cids, &t);
+        }
+      }
+
+      struct crystal cr;
+      crystal_init(&cr, &c);
+      sarray_transfer(struct cmp_t, &cids, p, 1, &cr);
+
+      // find unique components and number them
+      sarray_sort(struct cmp_t, cids.ptr, cids.n, c, 0, bfr);
+      uint cnt = 0;
+      if (cids.n > 0) {
+        cnt++;
+        struct cmp_t *pc = (struct cmp_t *)cids.ptr;
+        for (uint i = 1; i < cids.n; i++) {
+          if (pc[i].c > pc[i - 1].c)
+            cnt++;
+        }
+      }
+
+      in = cnt;
+      comm_scan(out, &c, gs_long, gs_add, &in, 1, wrk);
+      ulong s = out[0][0];
+      ncg = out[1][0];
+
+      if (cids.n > 0) {
+        struct cmp_t *pc = (struct cmp_t *)cids.ptr;
+        pc[0].uid = s;
+        for (uint i = 1; i < cids.n; i++) {
+          if (pc[i].c > pc[i - 1].c)
+            s++;
+          pc[i].uid = s;
+        }
+      }
+
+      sarray_transfer(struct cmp_t, &cids, p, 0, &cr);
+      crystal_free(&cr);
+
+      sarray_sort(struct cmp_t, cids.ptr, cids.n, c, 0, bfr);
+      for (uint e = 0; e < unmkd; e++) {
+        if (p[e * nv + 0] > -1) {
+          t.c = p[e * nv + 0];
+          sint uid = find_or_insert(&cids, &t);
+          assert(uid > -1);
+          component[inds[e]] = uid;
+        }
+      }
+
+      array_free(&cids);
+    }
+    comm_free(&c);
+
+    comm_allreduce(ci, gs_long, gs_max, &nnzg, 1, &wrk);
+    nmkd += nnzg;
+    comm_allreduce(ci, gs_long, gs_max, &ncg, 1, &wrk);
+    nc += ncg;
+  } while (nmkd < nelg);
+
+  free(p), free(ids), free(inds);
+  if (null_input == 1)
+    free(component);
+
+  return nc;
+}
+
 int balance_partitions(struct array *elements, int nv, struct comm *lc,
                        struct comm *gc, int bin, buffer *bfr) {
   assert(check_bin_val(bin, gc) == 0);
@@ -233,11 +432,8 @@ int balance_partitions(struct array *elements, int nv, struct comm *lc,
                   bfr);
   }
 
-#if 0
   nelt = elements->n;
-  sint ncomp = get_components(NULL, elements->ptr, lc, bfr, nelt, nv);
-  metric_acc(RSB_COMPONENTS, ncomp);
-#endif
+  sint ncomp = get_components_v2(NULL, nelt, nv, elements->ptr, gc, bfr);
 
   free(ids);
   gs_free(gsh);
@@ -251,7 +447,8 @@ int repair_partitions(struct array *elements, int nv, struct comm *tc,
 
   struct rsb_element *e = elements->ptr;
   sint *cmpids = tcalloc(sint, nelt);
-  sint ncomp = get_components(cmpids, e, tc, bfr, nelt, nv);
+  // sint ncomp = get_components(cmpids, e, tc, bfr, nelt, nv);
+  sint ncomp = get_components_v2(cmpids, nelt, nv, e, tc, bfr);
 
   slong ncompg = ncomp, buf;
   comm_allreduce(lc, gs_long, gs_max, &ncompg, 1, &buf);
@@ -328,7 +525,10 @@ int repair_partitions(struct array *elements, int nv, struct comm *tc,
 
     nelt = elements->n;
     cmpids = trealloc(sint, cmpids, nelt);
-    ncompg = ncomp = get_components(cmpids, elements->ptr, tc, bfr, nelt, nv);
+    // ncomp = get_components(cmpids, elements->ptr, tc, bfr, nelt, nv);
+    // ncompg =  ncomp
+    ncomp = get_components_v2(cmpids, nelt, nv, elements->ptr, tc, bfr);
+    ncompg = ncomp;
     comm_allreduce(lc, gs_long, gs_max, &ncompg, 1, &buf);
 
     free(cmpcnt);
