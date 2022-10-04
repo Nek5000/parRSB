3fcd6e4386b24466b775b216e5fc8a77a591a9ae
Thilina Rathnayake <thilinarmtb@gmail.com>
2022-10-03 21:33:49 -0400

    Get rid of sort-local.c

--- src/sort.c
+++ src/sort.c
@@ -2,8 +2,6 @@
 #include <float.h>
 #include <math.h>
 
-#define min(a, b) ((a) < (b) ? (a) : (b))
-
 static double get_scalar(struct array *a, uint i, uint offset, uint usize,
                          gs_dom type) {
   char *v = (char *)a->ptr + i * usize + offset;
@@ -105,6 +103,44 @@ static int set_bin(uint **proc_, struct sort *s, uint field, struct comm *c) {
   } while (id < np && index < size);
   for (; index < size; index++)
     proc[index] = np - 1;
+}
+
+static int sort_field(struct array *arr, size_t usize, gs_dom t, uint off,
+                      buffer *buf, int keep) {
+  uint nunits = arr->n;
+  void *ptr = arr->ptr;
+  switch (t) {
+  case gs_double:
+    gslib_sortp_double(buf, keep, (double *)((char *)ptr + off), nunits, usize);
+    break;
+  case gs_float:
+    gslib_sortp_float(buf, keep, (float *)((char *)ptr + off), nunits, usize);
+    break;
+  case gs_long: // FIXME gs_ulong
+    gslib_sortp_ull(buf, keep, (ulong *)((char *)ptr + off), nunits, usize);
+    break;
+  case gs_int: // FIXME gs_uint
+    gslib_sortp_ui(buf, keep, (uint *)((char *)ptr + off), nunits, usize);
+    break;
+  default:
+    break;
+  }
+
+  return 0;
+}
+
+int sort_local(struct sort *s) {
+  struct array *a = s->a;
+  buffer *buf = s->buf;
+  size_t usize = s->unit_size;
+  int i = s->nfields - 1;
+
+  sort_field(a, usize, s->t[i], s->offset[i], buf, 0), i--;
+  while (i >= 0)
+    sort_field(a, usize, s->t[i], s->offset[i], buf, 1), i--;
+  sarray_permute_buf_(s->align, usize, a->ptr, a->n, buf);
+
+  return 0;
 }
 
 static int parallel_bin_sort(struct sort *s, struct comm *c) {
@@ -348,5 +384,3 @@ int parallel_sort_private(struct sort *data, struct comm *c) {
 
   return 0;
 }
-
-#undef min
